f08db2de8e06625afe9aae5aca41732761d0f342
Peng-AP <pzq0406@gmail.com>
2026-04-06 13:54:07 -0400

    implemented brew and TODOs

--- CommitCafe.java
+++ CommitCafe.java
@@ -1,13 +1,16 @@
 public class CommitCafe {
 
-    private static String barista = "Nadine"; // STUDENT_TODO_1A: Change name
-    // STUDENT_TODO_1B: Add a nickname or title for the barista
+    private static String barista = "Aaron"; // STUDENT_TODO_1A: Change name
+    private static String nickname = "AA"; // STUDENT_TODO_1B: Add a nickname or title for the barista
 
     private static int cups = 0;
 
     public static void brew(String drink) {
         // STUDENT_TODO_2A: Implement - add 1 to cups & print a message
+	cups += 1;
+	System.out.println("Brewed one " + drink);
         // STUDENT_TODO_2B: Add a second brew-related improvement
+	System.out.println("Have a nice day!");
     }
 
     public static void printSummary() {
